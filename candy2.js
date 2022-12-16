d5272ef8a0e9f5228c6026dba60ca335c0f67d0b
l7ana <liana@saedesigngroup.com>
2022-12-15 15:10:32 -1000

    refines physics

--- candy2.js
+++ candy2.js
@@ -3,17 +3,26 @@ class Candy2 extends Block {
     super(world, attributes, options);
   }
 
-  addBody() {
-    this.body = Matter.Bodies.rectangle(this.attributes.x, this.attributes.y, this.attributes.w, this.attributes.h, this.options, this.attributes.image);
 
-    // this.attributes.image = candyImg;
+  updateShape() {
+    let mouseDistance = int(dist(this.body.position.x, this.body.position.y, mouseX, mouseY)); // check the distance from your mouse to the shape
+    if (mouseDistance <= mouseThreshold) { // if your mouse gets closer than the threshold...
+      this.body.position.x += random(-moveDistance, moveDistance); // give the shape a new x position
+      this.body.position.y += random(-moveDistance, moveDistance); // and a new y position
+    };
+  }
+
+  addBody() {
 
     this.options = {
-      friction: 0.5,
-      restitution: 0.1,
+      friction: 0.7,
+      restitution: 0.07,
       density: 1,
+      frictionAir: .05,
       angle: random(0, 360)
     };
+
+    this.body = Matter.Bodies.rectangle(this.attributes.x, this.attributes.y, this.attributes.w, this.attributes.h, this.options, this.attributes.image);
 
     this.show = function() {
       var pos = this.body.position;
@@ -30,6 +39,6 @@ class Candy2 extends Block {
       rect(0, 0, this.w, this.h);
       pop();
     };
-
   }
+
 }
--- sketch.js
+++ sketch.js
@@ -13,23 +13,14 @@ var Engine = Matter.Engine,
 
 var engine;
 var world;
-var boxes = [];
-var wall1;
-var wall2;
-var wall3;
-var wall4;
 var candy2;
 var candies = []; // an array that will hold our shapes
 let candyImg;
-let mouse;
 var block;
-let mouseConstraint;
 let shapes = []; // an array that will hold our shapes
-let numberOfShapes = 50; // how many shapes to draw
+let numberOfShapes = 100; // how many shapes to draw
 let mouseThreshold = 50; // how close can your mouse get to a shape before it moves
-let moveDistance = 150; // how far shapes move away from your mouse
-let animateDistance = 50; // how much each shape animates 
-
+let moveDistance = 10; // how far shapes move away from your mouse
 
 let height = window.innerHeight * .75;
 let width = window.innerWidth * .75;
@@ -43,49 +34,19 @@ function setup() {
   engine = Engine.create();
   world = engine.world;
   engine.world.gravity.scale = 0;
-  
-  // Static Elements
-  var options = {
-    isStatic: true,
-    render: {
-      fillStyle: red
-    }
-  };
-
-  wall1 = Bodies.rectangle(width / 2, 10, width, 10, options)
-  wall2 = Bodies.rectangle(width / 2, height + 10, width, 10, options)
-  wall3 = Bodies.rectangle(0 - 10, height / 2, 10, height, options)
-  wall4 = Bodies.rectangle(width + 10, height / 2, 10, height, options)
-
-  World.add(world, [wall1, wall2, wall3, wall4]);
-
 
     // create a bunch of shape objects
     for (let i = 0; i < numberOfShapes; i++) {
-      candies.push(new Candy2(world, {x: random(0, width), y: random(0, height), w: 150, h:50, image: candyImg}));
+      candies.push(new Candy2(world, {x: random(0, width), y: random(0, height), w: 150, h:50}));
     };
-
-    console.log(candies[1]);
-  }
-
-  function mousePressed() {
-  for (var i = 0; i < candies.length; i++) {
-    let mouseDistance = int(dist(this.x, this.y, mouseX, mouseY)); // check the distance from your mouse to the shape
-    if (mouseDistance <= mouseThreshold) { // if your mouse gets closer than the threshold...
-      candies[i].body.position.x += random(-moveDistance, moveDistance); // give the shape a new x position
-      candies[i].body.position.y += random(-moveDistance, moveDistance); // and a new y position
-      //this.x = lerp(this.x, random(this.x - moveDistance, this.x + moveDistance), 0.5);
-      //this.y = lerp(this.y, random(this.y - moveDistance, this.y + moveDistance), 0.5);
-    }
-  };
-    console.log(mouseX);
+    console.log(candies[1].body.friction);
   }
 
 function draw() {
   background(245);
   Engine.update(engine);
   for (var i = 0; i < candies.length; i++) {
+    candies[i].updateShape();
     candies[i].draw();
   };
-  rect(0, 10, width, 10)
 }
